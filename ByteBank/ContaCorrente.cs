cd911de13defced078b2bea82dac561b72f4bc1a
Wilber Ribeiro <wilbergoias@gmail.com>
2022-10-04 20:50:17 -0300

    Adição método Perfil()
    Método Perfil que mostra todas as informações da conta do usuário.

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -6,7 +6,7 @@
         public string conta;
         public int nAgencia;
         public string agencia;
-        public double saldo;
+        public double? saldo;
 
         public bool Sacar(double valor)
         {
@@ -37,6 +37,34 @@
                 return true;
             }
         }
+        public bool Transferir(double valor, ContaCorrente destinatario)
+        {
+            if (valor > saldo)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                destinatario.saldo += valor;
+                return true;
+            }
+        }
+        public void Perfil()
+        {
+            Console.WriteLine("\nSeus Dados:\n");
+            Console.WriteLine("Nome do Titular: "+ titular);
+            Console.WriteLine("Conta: " + conta);
+            Console.WriteLine("Número da Agência: "+ nAgencia);
+            Console.WriteLine("Agência: " + agencia);
+            Console.WriteLine("Saldo: "+ saldo);
+            
+        }
+
     }
 
 
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -7,12 +7,40 @@ conta1.titular = "Wilber Ribeiro";
 conta1.conta = "106-wr";
 conta1.nAgencia = 52;
 conta1.agencia = "Unica";
-conta1.saldo = 225.88;
+conta1.saldo = 225;
 
+ContaCorrente conta2 = new ContaCorrente();
+conta2.titular = "Julio Moura";
+conta2.conta = "107-JM";
+conta2.nAgencia = 52;
+conta2.agencia = "Unica";
+conta2.saldo = 0;
+
+//Saca da conta do Wilber
 Console.WriteLine("Saldo atual: " + conta1.saldo);
 conta1.Sacar(203);
 Console.WriteLine("Saldo atual: " + conta1.saldo);
 
-conta1.Depositar(-30);
+//Deposita na conta do Wilber
+conta1.Depositar(30);
 Console.WriteLine("Saldo atual: " + conta1.saldo);
+
+//Transfere da conta do Wilber para a conta do Julio
+bool transferencia = conta1.Transferir(50, conta2);
+
+//Confere se a transferencia foi dada
+if (transferencia)
+{
+    Console.WriteLine("Tranferência concluída com sucesso");
+}
+else
+{
+    Console.WriteLine("Transferência não autorizada.");
+}
+Console.WriteLine("Saldo atual(1): " + conta1.saldo);
+Console.WriteLine("Saldo atual(2): " + conta2.saldo);
+
+conta1.Perfil();
+
+
 Console.ReadKey();
